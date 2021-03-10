01acad06a9ded964692ce8452da558bc0cbe0d36
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-10 20:01:26 +0100

    Generando formulario con conexion a BD

--- Ejercicios_Clase2/upload.php
+++ Ejercicios_Clase2/upload.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
-ini_set("display_errors",true);
+ini_set("display_errors", true);
 
-if ($_SERVER['REQUEST_METHOD']=='POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //var_dump($_FILES)
     $allowed_image_types = ['image/jpeg'];
     $image_info = getimagesize($_FILES['userfile']['tmp_name']);
@@ -17,13 +17,13 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
     } else {
         echo "Image uploaded correctly to $file_destination";
     }
-}else{
-    $form_html =<<<HTML
+} else {
+    $form_html = <<<HTML
 <form enctype="multipart/form-data" action="upload.php" method="post">
 <input type="file" name="userfile">
 <input type="hidden" name="MAX_FILE_SIZE" value="300000">
 <input type="submit" value="Upload">
 </form>
 HTML;
-echo $form_html;
+    echo $form_html;
 }
