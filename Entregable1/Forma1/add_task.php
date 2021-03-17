5a0499813e742ba83de44dd4d53618dc6718494f
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-17 17:27:09 +0100

    Agregando tareas a un txt

--- Entregable1/Forma1/add_task.php
+++ Entregable1/Forma1/add_task.php
@@ -11,6 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!empty($_POST['task'])) {
         $task = filter_var($_POST['task'], FILTER_SANITIZE_STRING);
         $show_task = file_put_contents('task.txt',$task);
+        $fp = fopen('task.txt', 'w');
+        fwrite($fp, $task;
+        fwrite($fp, $task);
+        fclose($fp);
         $output .= $task . "<br>";
         echo $show_task;
     }
