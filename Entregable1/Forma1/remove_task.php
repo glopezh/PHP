f44b8126aff624de9570ba28e7a531838a28385a
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-17 18:09:24 +0100

    Agregando tareas a un txt

--- Entregable1/Forma1/remove_task.php
+++ Entregable1/Forma1/remove_task.php
@@ -1,2 +1,61 @@
 /*Al borrar una tarea, la guarde en otro fichero en disco de tareas completadas. Asi podremos
 tener un fichero con las tareas todavia pendientes, y otro con las tareas ya realizadas*/
+
+<?php
+/* Archivo que se ejecute por linea de comandos y que recibe como párametro una cadena
+de texto con la tarea que queremos añadir a nuestra lista de tareas, como por ejemplo
+"Ir al gymnasio". Esta tarea se guardará en el archivo en el disco, que llevara el control
+de tareas pendientes*/
+error_reporting(E_ALL);
+ini_set("error_tasks", true);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $output = "";
+    if (!empty($_POST['task'])) {
+        $task = $_POST['task'];
+        $fp = fopen('task.txt', 'c+');
+        //$show_task = file_put_contents('task.txt',$task);
+        $fp =fopen("task.txt","w");
+        fwrite($fp,$task);
+        fclose($fp);
+        //$output.= $task.PHP_EOL;
+// Escribe el contenido al fichero
+        //file_put_contents($fichero, $actual);
+        //fclose($fp);
+        //$output .= $task . "<br>".PHP_EOL;
+        //echo $task.PHP_EOL;
+    }
+    echo file_get_contents('task.txt').PHP_EOL;
+    echo $output.PHP_EOL;
+} else {
+    $form_html = <<<HTML
+    <form action="remove_task.php" method="POST">
+     <fieldset>
+    <h1>List of tasks</h1>
+
+    <ol id="todo-list" class="todo-list">
+    
+      <li class="todo todo-completed" data-todo-id="123">
+        <input type="checkbox" checked>
+        <input class="todo-text" type="text" readonly value="Avanzar entregable 1">
+      </li>
+      
+      <li class="todo" data-todo-id="456">
+        <input type="checkbox">
+        <input class="todo-text" type="text" readonly value="Avanzar entregable 2">
+      </li>
+      
+      <li class="todo" data-todo-id="456">
+        <input type="checkbox">
+        <input name ="task" class="todo-text-edit" type="text" value="">
+      </li>
+      
+    </ol>
+    
+	    <fieldset class="form-actions">
+		    <input type="submit" value="Add task!" />
+	    </fieldset>
+    </form>
+HTML;
+    echo $form_html;
+}
