140e2b29b5b67b06ff72b0deb2dc3e21890cea94
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-17 18:16:38 +0100

    Generando el archivo data.txt

--- Ejemplos_uso_ficheros/fichero3.php
+++ Ejemplos_uso_ficheros/fichero3.php
@@ -3,6 +3,7 @@ $fp = fopen('data.txt', 'w');
 fwrite($fp, '1');
 fwrite($fp, '23');
 fclose($fp);
-echo file_get_contents('data.txt');
+$fichero = 'data.txt';
+echo file_get_contents($fichero);
 // the content of 'data.txt' is now 123 and not 23!
 ?>
