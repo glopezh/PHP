0cf92ce8fccd2b8413730135d8933d6f2400d4f1
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-17 15:59:00 +0100

    Probando abrir una tarea de un archivo

--- Entregable1/Forma1/display_tasks.php
+++ Entregable1/Forma1/display_tasks.php
@@ -1,2 +1,6 @@
 /* Archivo que al ejecutarse por linea de comandos nos permita eliminar una tarea pendiente de la lista
 guardada en el fichero. Este script recibe como parámetro una cadena de texto con la tarea a eliminar*/
+
+<?php
+
+echo file_get_contents(Forma1/'task.txt');
