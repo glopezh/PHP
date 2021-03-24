60fad5bf0a5210f0aad65d1bfbd99874e52365df
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-24 20:29:21 +0100

    Trabajando con composer

---
+++ Ejercicio_Tema5/src/index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(-1);
+ini_set("display_errors", "true");
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+require_once __DIR__ . '/vendor/autoload.php’;
+$log = new Logger('name’);
+$log->pushHandler(new StreamHandler('your.log', Logger::WARNING));
+// add record to the log
+$log->addRecord(Logger::WARNING, 'Foo1');
+$log->warning('Foo2');
+$log->error('Boo');
