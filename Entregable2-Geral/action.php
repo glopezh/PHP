4d696ced76c4746cca3125aa5ccc45cbc1f5be05
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-29 10:34:09 +0200

    Modificando salida de usuario y password

--- Entregable2-Geral/action.php
+++ Entregable2-Geral/action.php
@@ -6,11 +6,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $output = "";
     if (!empty($_POST['username'])) {
         $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-        $output .= "Se ingreso correctamente el username" . $username;
+        $output .= "Se ingreso correctamente el username" . $username.PHP_EOL;
     }
     if (!empty($_POST['password'])) {
         $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
-        $output .= "Se ingreso correctamente el password" . $password;
+        $output .= "Se ingreso correctamente el password" . $password.PHP_EOL;
     }
 echo $output;
 } else {
