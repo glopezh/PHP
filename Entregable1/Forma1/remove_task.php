43cff86488af303929759add52b20990ba5670ab
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-29 10:14:28 +0200

    Creando login

--- Entregable1/Forma1/remove_task.php
+++ Entregable1/Forma1/remove_task.php
@@ -6,10 +6,10 @@ tener un fichero con las tareas todavia pendientes, y otro con las tareas ya rea
 $file_task= "control_task.txt";
 $file_task_completed= "completed_task.txt";
 $read_task=file_get_contents($file_task);
-//var_dump($read_task);
+var_dump($read_task);
 $array = explode("\n", $read_task);
-//var_dump($array);
-//PROCESO DE ELIMINAR LA TAREA
+var_dump($array);
+/*//PROCESO DE ELIMINAR LA TAREA
 if (($task_completed = array_search("Do my homework", $array)) !== false) {
     unset($array[$task_completed]);
     var_dump($task_completed);
@@ -19,4 +19,4 @@ if (($task_completed = array_search("Do my homework", $array)) !== false) {
 echo "Tareas no completadas";
 echo file_get_contents($file_task).PHP_EOL;
 echo "Tareas completadas";
-echo file_get_contents($file_task_completed).PHP_EOL;
+echo file_get_contents($file_task_completed).PHP_EOL;*/
---
+++ Entregable2-Geral/index.php
@@ -0,0 +1,45 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $output = "";
+    if (!empty($_POST['username'])) {
+        $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $output .= "Se ingreso correctamente el username" . $username;
+    }
+    if (!empty($_POST['username'])) {
+        $password = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $output .= "Se ingreso correctamente el password" . $password;
+    }
+
+} else {
+    $form_html = <<<HTML
+<form action="action_page.php" method="post">
+<!--  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>-->
+
+  <div class="container">
+    <label for="username"><b>Username</b></label>
+    <input id ="text" type="text" placeholder="Enter Username" name="username" required  data-validate = "Username is required">
+
+    <label for="psw"><b>Password</b></label>
+    <input id ="password" type="password" placeholder="Enter Password" name="psw" required data-validate = "Password is required">
+
+    <button type="submit">Login</button>
+    
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+HTML;
+    echo $form_html;
+}
