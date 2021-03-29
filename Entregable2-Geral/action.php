eb01c35891967e1045a6ca291668c83215d7d5b3
Geraldine Lopez <geraldinelopz@gmail.com>
2021-03-29 10:24:17 +0200

    Eliminando boton cancelar y otros cambios

--- Entregable2-Geral/action.php
+++ Entregable2-Geral/action.php
@@ -29,14 +29,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <button type="submit">Login</button>
     
+    /*Se le debe de agregar una cookie para que guarde los datos de sesión*/
+    
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
+    
   </div>
 </form>
 
